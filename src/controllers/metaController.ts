3126e147ff637929677433ed44223b955b48ffdd
Audace SANGANO <audasang2018@gmail.com>
2026-08-12 14:50:25 +0200

    last today change

--- src/controllers/metaController.ts
+++ src/controllers/metaController.ts
@@ -3,5 +3,5 @@ import { sendResponse } from "../utils/apiResponse.js";
 import { PERMISSION_GROUPS } from "../utils/permissions.js";
 
 export async function listPermissionCatalog(_req: Request, res: Response): Promise<void> {
-  sendResponse(res, 200, "Permission catalog retrieved successfully", PERMISSION_GROUPS);
+  sendResponse(res, 200, "Permission catalog retrieved successfully", PERMISSION_GROUPS); 
 }
